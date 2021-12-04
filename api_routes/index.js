14d509ca9a67f0d5efd3418d278998898fa4614a
Laura Cole <lauracole1900@comcast.net>
2021-12-04 13:14:13 -0700

    added thoughtRoute & userRoute files; drafted api_routes/index

---
+++ api_routes/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const router = require("express").Router();
+const apiThought = require("./thoughtRoutes");
+const apiUser = require("./userRoutes");
+
+// API Routes
+router.use("/api/thought", apiThought);
+router.use("/api/user", apiUser);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
