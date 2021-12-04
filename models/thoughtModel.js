1826f42fc78f6adf1820bd9105dd728600c8a87f
Laura Cole <lauracole1900@comcast.net>
2021-12-03 23:06:14 -0700

    started working on thoughtModel

--- models/thoughtModel.js
+++ models/thoughtModel.js
@@ -20,3 +20,41 @@
 // **Schema Settings**:
 
 // Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const reactions = require("./reactionModel");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    thoughtReactions: [reactions]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.thoughtReactions?.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
